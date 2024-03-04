cb488a962d6a10da73addc7e2520591cbaf934f2
Ayushdhynai1947 <144094633+Ayushdhynai1947@users.noreply.github.com>
2024-03-04 11:43:54 +0530

    Create tempCodeRunnerFile.py

---
+++ loops/tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+#basic syntax for loop 
+class loop():
+    def print_i(self):
+        fruits = ["apple", "peach ", "pear"]
+        for fruit in fruits:
+            print(fruit)
+            print(fruit + " pie")
+        
+
+
+a = loop()
+a.print_i()
