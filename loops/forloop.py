5c17d66f49ef2dea53ca189a676fbab20f482eee
Ayushdhynai1947 <144094633+Ayushdhynai1947@users.noreply.github.com>
2024-03-04 11:43:35 +0530

    Update forloop.py

--- loops/forloop.py
+++ loops/forloop.py
@@ -10,3 +10,26 @@ class loop():
 
 a = loop()
 a.print_i()
+
+
+
+
+
+#  exercise: calculate a avergage height of student in list
+class avg_height():
+    
+    def __init__(self) -> None:
+        student_height = input("Input a list of student height ").split()
+        for n in range(0,len(student_height)):
+            student_height[n] = int(student_height[n])
+        print(student_height)
+
+        total_height = sum(student_height)
+        number_of_student = len(student_height)
+        average_height = round(total_height/number_of_student)
+        print(f"average heigth of student is {average_height}")
+    
+a = avg_height()
+
+
+
