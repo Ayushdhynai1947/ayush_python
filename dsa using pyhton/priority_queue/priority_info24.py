ae0d8c811ef75788ec6754159868349d351511a3
Ayushdhynai1947 <144094633+Ayushdhynai1947@users.noreply.github.com>
2024-04-05 01:27:01 +0530

    priority

---
+++ dsa using pyhton/priority_queue/priority_info24.py
@@ -0,0 +1,23 @@
+""" priority Queue"""
+""" a priority queue is a collection of elemebts such that each has been 
+    assigned a priority 
+     the order of elements are deleted and processed comes from the following rules"""
+     
+# an elments of highest priority is processed is processed before any elemnts of lower priority
+
+# two elements with the same priority are processed according to the  order in which thet are added in the priority queue
+     
+     
+   # opertaion on priority queue  
+   # push()
+   # pop()
+   # is_empty()
+   #size()
+   
+   
+   
+# implementaion of priority queues
+# using link list
+# using list
+# using heap 
+     
---
+++ dsa using pyhton/priority_queue/priority_queue_list25..py
@@ -0,0 +1,9 @@
+class  PrirorityQueue():
+    def __init__(self) -> None:
+        self.list =[]
+        
+    def push(self,data,priority):
+        index =0
+        
+        
+    
