cc60a1ab3ff24086d1f41bdd1c0660f04202e53b
Ayushdhynai1947 <144094633+Ayushdhynai1947@users.noreply.github.com>
2024-04-06 01:13:14 +0530

    Update priority_queue_linklist26.py

--- dsa using pyhton/priority_queue/priority_queue_linklist26.py
+++ dsa using pyhton/priority_queue/priority_queue_linklist26.py
@@ -17,5 +17,36 @@ class PriorityQueue:
         else:
             temp = self.start
             while temp.next and temp.next.priority<=priority:
+                temp = temp.next
+            n.next = temp.next
+            temp.next =n
+        self.item_count+=1
+    def is_empty(self):
+        return self.start ==None
+    
+    def pop(self):
+        if self.is_empty():
+            raise IndexError('priority queue is empty')
         
+        data = self.start.item
+        self.start = self.start.next
+        self.item_count-=1
+        return data
+    
+    def size(self):
+        return self.item_count
+    
+p =PriorityQueue()
+p.push('amil',4)
+p.push('arjun',7)
+p.push('ashima',2)
+p.push('Agrah',5)
+p.push('Anant',8)
+p.push('Ambika',1)
+
+
+while not p.is_empty():
+    print(p.pop())
+
+
     
