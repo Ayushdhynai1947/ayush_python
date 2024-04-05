004ff3b6acb10adb2ff0f48fff424bdd08d60a15
Ayushdhynai1947 <144094633+Ayushdhynai1947@users.noreply.github.com>
2024-04-05 21:31:51 +0530

    Update priority_queue_linklist26.py

--- dsa using pyhton/priority_queue/priority_queue_linklist26.py
+++ dsa using pyhton/priority_queue/priority_queue_linklist26.py
@@ -3,3 +3,19 @@ class node :
         self.item = item
         self.priority =priority
         self.next =next
+
+class PriorityQueue:
+    def __init__(self) -> None:
+        self.start = None
+        self.item_count =0
+        
+    def push(self,data,priority):
+        n = node(data,priority)
+        if not self.start or priority <self.start.priority:
+            n.next = self.start
+            self.start =n
+        else:
+            temp = self.start
+            while temp.next and temp.next.priority<=priority:
+        
+    
