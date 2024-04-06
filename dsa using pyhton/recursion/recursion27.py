b31f5278f769a2062ddd8312f39d15d6389f3c86
Ayushdhynai1947 <144094633+Ayushdhynai1947@users.noreply.github.com>
2024-04-06 14:33:16 +0530

    done

---
+++ dsa using pyhton/recursion/recursion27.py
@@ -0,0 +1,10 @@
+# recursion : when a function call itself  again and againis called recursion
+
+def f1(n):
+    if n ==1:
+        return 1
+    s = n + f1(n-1)
+    return s
+
+r= f1(3)
+print(r)
