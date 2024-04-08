8ea94708f4bee210476c73646b25051ea085567d
Ayushdhynai1947 <144094633+Ayushdhynai1947@users.noreply.github.com>
2024-04-08 19:35:30 +0530

    Update tree30.py

--- dsa using pyhton/non_liner_data_structure/tree30.py
+++ dsa using pyhton/non_liner_data_structure/tree30.py
@@ -4,3 +4,21 @@
             there is a special node called thr root node of the tree and the remaining nodes are partitioned into
             n>0 disjoint subsets ,each of which
 """
+
+
+
+""" binary search  tree: a binary search tree is define as afinite set if elements , called nodes ,such that 
+ - T is empty (called the null tree or empty tree), or
+ -  t contains a distinguisshed node R called the root if t1 and the remianing nides of T form an remaing 
+    nodes T forms ab order pairs of disjoint binary trees T1 and t2
+                          
+
+Binary searching tree:
+                        A binary search tree is the most important data structure that enable one to search 
+                        for and find an elemnts with an averag runnig times
+                        f(n) = O(log2n)
+                        
+                        douplicate value are not allowed in BST(By default)
+                                                
+                          
+                          """
