7a728dff772320179663c7fd424c617fd97001ad
Ayushdhynai1947 <144094633+Ayushdhynai1947@users.noreply.github.com>
2024-04-21 20:16:19 +0530

    done

--- question/1.py
+++ question/1.py
@@ -34,8 +34,42 @@ t1 =tuple([int(e) for e in input().split(",")] )
 t1 =tuple()
 t2 = tuple([1,2,3])
 t3 = tuple(range(5))
+#######################
+############################
+
+def f1():
+    print("enter a number")
+    n = int(input())
+    for e in range(1,n+1):
+        print(e**2, end='')
+    print()
+    
+
+f1()
 
 
 
+
+#########recusrion funtion ###########
+def f1(n):
+    if n ==1:
+        return 1
+    
+    s  = n +f1(n-1)
+    return s
+
+f1(4)
+        
+        
+        
+        
+##############factorail using recursion ############
+def fact(n):
+    if n==0 or n==1:
+        return 1
+    return n*fact(n-1)
+
+
+f = lambda n: 1 if n ==0 or n==1 else n*f(n-1)
 
 
--- question/tempCodeRunnerFile.py
+++ question/tempCodeRunnerFile.py
@@ -1,4 +1,9 @@
-t1 =tuple([int(e) for e in input().split(",")] )
-t1 =tuple()
-t2 = tuple([1,2,3])
-t3 = tuple(range(5))
+def f1(n):
+    if n ==1:
+        return 1
+    
+    s  = n +f1(n-1)
+    return s
+
+f1(4)
+        
