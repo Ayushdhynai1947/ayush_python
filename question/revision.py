3f5f16ad9ef32781ad5e3ad733b0c270d7ee7e4a
Ayushdhynai1947 <144094633+Ayushdhynai1947@users.noreply.github.com>
2024-05-12 23:54:41 +0530

    pratice

--- question/revision.py
+++ question/revision.py
@@ -208,4 +208,37 @@ print(s4)
 
 
 
-###################################################################
+#################funtiona overloding##################################################
+class O1 :
+    
+    
+    def call(self):
+        print("calling 15")
+        
+    
+    def getNotification(self):
+        print("Notification on top 15")
+        
+        
+class OS2(O1):
+    def getNotification(self):
+        print("notification at the bottom ,16")
+        
+        
+class IPHONE:
+    
+    def getOSVersion(self,os):
+        os.getNotification()
+    
+    # def call(self,os):
+    #     os.call()
+
+    
+    
+        
+
+a1 =OS2()
+# a1.getNotification()
+
+a2 = IPHONE()
+a2.getOSVersion(a1)
--- question/tempCodeRunnerFile.py
+++ question/tempCodeRunnerFile.py
@@ -1,31 +1,35 @@
 
-
-class A:  
-    def __init__(self,m1 ,m2) -> None:
-        self.m1 = m1
-        self.m2 = m2
+#################funtiona overloding##################################################
+class O1 :
+    
+    
+    def call(self):
+        print("calling 15")
         
-    def __add__(self,other):
-        total_m1 = self.m1 + other.m1
-        total_m2 = self.m2 + other.m2 
-        s3 = A(total_m1,total_m2)
-        return s3
     
-    def __sub__ (self,other):
-        total_m1 = self.m1 - other.m1
-        total_m2 = self.m2 - other.m2
-        s4 = A(total_m1,total_m2)
-        return s4
-      
-    def __str__(self) -> str:
-        return str(self.m1) + ":"+ str(self.m2)  
+    def getNotification(self):
+        print("Notification on top 15")
+        
+        
+class OS2(O1):
+    def getNotification(self):
+        print("notification at the bottom ,16")
+        
+        
+class IPHONE:
     
-     
-c1 = A(23,45)
-c2 = A(54,76)
+    def getOSVersion(self,os):
+        os.getNotification()
+    
+    # def call(self,os):
+    #     os.call()
 
-s3 = c1 + c2    # A.__add__(s1,s2)
-               # s1.__add__(s2)
-s4 = c1 - c2
-print(s3)
-print(s4)
+    
+    
+        
+
+a1 =OS2()
+# a1.getNotification()
+
+a2 = IPHONE()
+a2.getOSVersion(a1)
