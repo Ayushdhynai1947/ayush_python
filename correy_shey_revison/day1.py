ebfdf1e9c1e4ab29488a973281f70757b00fd65b
Ayushdhynai1947 <144094633+Ayushdhynai1947@users.noreply.github.com>
2024-07-13 00:09:48 +0530

    Create day1.py

---
+++ correy_shey_revison/day1.py
@@ -0,0 +1,9 @@
+courses =['History','Maths','Physics','CompSci']
+courses_2 = ['art','education']
+courses.insert(0,courses_2)
+courses.remove('Maths')
+courses.sort()
+nums= [1,3,2,5,7,6]
+nums.sort()
+
+print(courses)
